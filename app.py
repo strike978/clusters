068e3ae42d9367c6cc896dd918ca628b86699788
Omar Nunez <onunez978@gmail.com>
2023-03-04 17:14:52 -0500

    changed output

--- app.py
+++ app.py
@@ -1,67 +1,61 @@
-import io
 import pandas as pd
+from scipy.cluster.hierarchy import linkage, fcluster
 import streamlit as st
-from sklearn.cluster import AgglomerativeClustering, KMeans
+import io
 
-# Define the app interface
-st.set_page_config(layout="wide", page_title="G25 Clustering App")
-st.title("G25 Clustering App")
+# Setting the layout of the page to wide and the title of the page to G25 Clustering.
+st.set_page_config(layout="wide", page_title="G25 Clustering")
+st.title("G25 Clustering")
 
-# Load default data from "africa.csv"
-with open("africa.csv", "r") as f:
-    default_csv_data = f.read()
-
-# Add a file uploader to change the input data
+# Creating a file uploader to upload data as CSV or text
 uploaded_file = st.file_uploader(
-    "Upload CSV or TXT file:", type=["csv", "txt"])
+    "Upload a CSV or text file", type=["csv", "txt"])
 
+# Reading the data from the file africa.csv and displaying it in the text area.
+default_data = open("africa.csv", "r").read()
 if uploaded_file is not None:
-    # Load data from uploaded file
-    csv_data = uploaded_file.read().decode("utf-8")
-else:
-    # Use default data from "africa.csv"
-    csv_data = default_csv_data
+    default_data = uploaded_file.getvalue().decode('utf-8')
 
-# Add a text input box for the user to edit the CSV data
-csv_data = st.text_area("Edit CSV data:", value=csv_data, height=300)
+data_input = st.text_area("Enter data in CSV format", value=default_data)
 
-# Add a row with clustering method and number of clusters
-col1, col2 = st.columns(2)
-with col1:
-    clustering_method = st.selectbox("Clustering method:", ["Ward", "K-means"])
-with col2:
-    n_clusters = st.number_input(
-        "Number of clusters:", min_value=2, max_value=64, value=3)
+# Reading the data from the file africa.csv and displaying it in the text area.
+if data_input:
+    data = pd.read_csv(io.StringIO(data_input), header=None).iloc[:, 1:]
+    populations = pd.read_csv(io.StringIO(
+        data_input), header=None, usecols=[0])[0]
 
-# Add a button to perform clustering and display results
-if st.button("Cluster populations"):
-    # Load data from CSV data
-    data = pd.read_csv(io.StringIO(csv_data), header=None)
-    data.set_index(data.columns[0], inplace=True)
+    # This is the code that is used to cluster the data.
+    num_clusters = st.number_input(
+        "Number of clusters", min_value=2, max_value=len(data), value=2)
+    clustering_method = "ward"
 
-    # Perform clustering using the selected method
-    if clustering_method == "Ward":
-        cluster = AgglomerativeClustering(
-            n_clusters=n_clusters, linkage="ward")
-    else:
-        cluster = KMeans(n_clusters=n_clusters)
-    cluster.fit(data)
+    linkage_matrix = linkage(data, method='ward')
+    cluster_assignments = fcluster(
+        linkage_matrix, num_clusters, criterion='maxclust')
 
-    # Create a dictionary of population lists, grouped by cluster
-    pop_clusters = {i+1: [] for i in range(cluster.n_clusters)}
-    for pop, c in zip(data.index, cluster.labels_):
-        pop_clusters[c+1].append(pop)
+    # Concatenating the populations, cluster assignments and data.
+    results = pd.concat(
+        [populations, pd.Series(cluster_assignments), data], axis=1)
+    results.columns = ['Population', 'Cluster'] + \
+        [f"Feature {i}" for i in range(len(data.columns))]
+    results_sorted = results.sort_values('Cluster')
 
-    # Print the populations in each cluster
-    output = f"Clustering method: {clustering_method}\n\n"
-    for c, pops in pop_clusters.items():
-        output += f"Cluster {c}: {', '.join(pops)}\n"
-        if c < len(pop_clusters):
-            output += "\n"
-    st.code(output)
+    output_str = ""
+    for i in range(len(results_sorted)):
+        population_str = ':'.join(
+            results_sorted.iloc[i]['Population'].split(':')[:4])
+        country_str = results_sorted.iloc[i]['Population'].split(':')[-1]
+        feature_str = ','.join([str(val)
+                               for val in results_sorted.iloc[i][2:]])
+        output_str += f"{results_sorted.iloc[i]['Cluster']}:{population_str}*({country_str}),{feature_str}\n"
 
-    # Add a download button for the output
-    file_name = f"clustering_output_{clustering_method}_{n_clusters}.txt"
-    file_bytes = output.encode("utf-8")
-    st.download_button(label="Download output",
-                       data=file_bytes, file_name=file_name)
+    # This is the code that is used to download the results of the clustering.
+    st.text_area("Output", value=output_str)
+
+    with io.BytesIO(output_str.encode()) as buffer:
+        st.download_button(
+            label=f"Download Results ({num_clusters} Clusters) as File",
+            data=buffer.getvalue(),
+            file_name=f"results_{num_clusters}_clusters.txt",
+            mime="text/plain"
+        )
